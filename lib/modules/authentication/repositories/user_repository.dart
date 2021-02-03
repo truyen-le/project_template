0792275ef9585db6e54f010c632b05b50b676cd3
Truyen Le <truyen.le@manifera.com>
2021-02-03 17:03:39 +0700

    Create mock code for authentication user

---
+++ lib/modules/authentication/repositories/user_repository.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:project_template/modules/authentication/models/user.dart';
+import 'package:uuid/uuid.dart';
+
+
+class UserRepository {
+  User _user;
+
+  Future<User> getUser() async {
+    if (_user != null) return _user;
+    return Future.delayed(
+      const Duration(milliseconds: 300),
+          () => _user = User(Uuid().v4()),
+    );
+  }
+}
